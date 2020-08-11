fbaa91315ac8057a1b1113b24d8254ebb3a98cee
syfweb1 <syfweb@163.com>
2020-08-11 21:45:52 +0800

    egit 项目提交

---
+++ ssa-gittest/src/main/java/ssa/web/gittest/App.java
@@ -0,0 +1,13 @@
+package ssa.web.gittest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
